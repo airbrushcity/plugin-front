a4220a8a84502c1f30247f5d9aae967f8915ff7a
airbrushcity <a.kolbfleisch@airbrush-city.de>
2021-06-24 15:24:43 +0200

    Update Static Pages
    Static Page Overrides

--- src/Providers/FrontServiceProvider.php
+++ src/Providers/FrontServiceProvider.php
@@ -45,6 +45,14 @@ class FrontServiceProvider extends TemplateServiceProvider
 		$this->overrideTemplate("Ceres::Item.SingleItemWrapper", "Front::Item.SingleItemWrapper");
 		$this->overrideTemplate("Ceres::Item.SingleItemView", "Front::Item.SingleItemView");
 		
+		$this->overrideTemplate("Ceres::StaticPages.CancellationForm", "Front::StaticPages.CancellationForm");
+		$this->overrideTemplate("Ceres::StaticPages.CancellationRights", "Front::StaticPages.CancellationRights");
+		$this->overrideTemplate("Ceres::StaticPages.ItemNotFound", "Front::StaticPages.ItemNotFound");
+		$this->overrideTemplate("Ceres::StaticPages.LegalDisclosure", "Front::StaticPages.LegalDisclosure");
+		$this->overrideTemplate("Ceres::StaticPages.PageNotFound", "Front::StaticPages.PageNotFound");
+		$this->overrideTemplate("Ceres::StaticPages.PrivacyPolicy", "Front::StaticPages.PrivacyPolicy");
+		$this->overrideTemplate("Ceres::StaticPages.TermsAndConditions", "Front::StaticPages.TermsAndConditions");
+		
 		// $this->overrideTemplate("Ceres::Widgets.Header.TopBarWidget", "Front::Widgets.Header.CustomTopBarWidget");	
 		// $this->overrideTemplate("Ceres::Widgets.Item.OrderPropertyWidget", "Front::Widgets.Item.CustomOrderPropertyWidget");	
 
