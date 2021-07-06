90e327f624be8c8491a31c284d6f02806dc04ef2
airbrushcity <a.kolbfleisch@airbrush-city.de>
2021-07-06 12:21:55 +0200

    Update to right defaults

--- resources/js/src/app/components/itemList/CategoryItem.vue
+++ resources/js/src/app/components/itemList/CategoryItem.vue
@@ -107,7 +107,7 @@
                     </add-to-basket>
 
                     <div class="vat small text-muted">
-                        ** Versandgewicht: {{ variation.weightG }} Gramm. 
+                        ** Versandgewicht: {{ currentVariation.variation.weightG }} Gramm. 
                     </div>
                 </div>
             </slot>
